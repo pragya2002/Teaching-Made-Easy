5f86b0316141a14415ad8e5a06cf4819cca3fb7c
Pragya Awasthi <pragya@Pragyas-MacBook-Air.local>
2021-10-18 04:07:19 +0530

    newest

---
+++ online evaluator/Modules/configurations.py
@@ -0,0 +1,8 @@
+config = {
+    "apiKey": "AIzaSyD9aRQHR1t6xH2eZvWb_n1D5aAfmt5SZx8",
+    "authDomain": "neww-8199a.firebaseapp.com",
+    "databaseURL": "https://neww-8199a-default-rtdb.firebaseio.com/",
+    "projectId": "neww-8199a",
+    "storageBucket": "gs://neww-8199a.appspot.com",
+    "messagingSenderId": "733141394267"
+}
